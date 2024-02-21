bf9f724a870a2e9893263181770daf8d43e3c69e
Tuomas Reijonen <hture01@xamk.fi>
2024-02-21 12:52:18 +0200

    Add project files.

---
+++ PullRequestDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PullRequestDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
